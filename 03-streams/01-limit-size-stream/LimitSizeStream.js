2b0b72da04cd1ab3fe40e0e41d6341f6f42201ca
SolArt <hound2014@gmail.com>
2021-06-26 23:49:05 +0600

    LimitSizeStream and LineSplitStream implemented

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,24 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
-  constructor(options) {
+  constructor({limit, ...options}) {
     super(options);
+    this._limit = limit;
+    this._size = 0;
+  }
+
+  _addSize(val) {
+    this._size += val;
   }
 
   _transform(chunk, encoding, callback) {
-
+    const size = Buffer.byteLength(chunk);
+    if (size + this._size <= this._limit) {
+      this._addSize(size);
+      callback(null, chunk);
+    } else {
+      callback(new LimitExceededError());
+    }
   }
 }
 
