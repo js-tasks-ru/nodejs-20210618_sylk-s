e91b0e45d9adaa0bcef341e3a8614ac2400339ef
SolArt <hound2014@gmail.com>
2021-07-05 14:30:09 +0600

    File-server-get implemented

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,23 +1,48 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
-
   switch (req.method) {
     case 'GET':
-
+      sendFile(req, res);
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
   }
 });
+
+const sendFile = (req, res) => {
+  const pathname = url.parse(req.url).pathname.slice(1);
+
+  // Check if url is nested
+  if (/\//.test(pathname)) {
+    res.statusCode = 400;
+    res.end('Nested folders are not supported');
+    return;
+  }
+
+  const filepath = path.join(__dirname, 'files', pathname);
+  const stream = fs.createReadStream(filepath);
+  stream.pipe(res);
+
+  stream.on('error', (e) => {
+    if (e.code === 'ENOENT') {
+      res.statusCode = 404;
+      res.end('File not found');
+    } else {
+      res.statusCode = 500;
+      res.end('Internal server error');
+    }
+  });
+
+  req.on('aborted', () => {
+    stream.destroy();
+  });
+};
 
 module.exports = server;
